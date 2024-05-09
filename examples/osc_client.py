bb946b7983bd9178aeaed5280c7f719fa7a0eb8e
Dominik Keusch <dominik.keusch@ipt.ch>
2024-05-09 15:08:38 +0200

    simple examples OSC client and server

---
+++ examples/osc_client.py
@@ -0,0 +1,26 @@
+"""Small example OSC client
+
+This example program asks for user input to send a message to a OSC channel
+"""
+import argparse
+import random
+import time
+
+from pythonosc import udp_client
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=8010,
+                        help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    print("The Server settings are: IP: ", args.ip, "   Port: ", args.port)
+
+    while True:
+        value = str(input("Add message you want to send in the format /channel;message\n"))
+        # Example: /medias/Text_Generator/Text;test
+        channel, text = value.split(";")
+        client.send_message(channel, text)
