a947cae68944707175f8eb11211525ce40f6df02
NOWandWOW <nowandwowgmbh@gmail.com>
2024-05-14 17:27:19 +0200

    add getting task list

--- robot.py
+++ robot.py
@@ -31,6 +31,18 @@ class Robot:
         response = send_get_request(url, auth=self.AUTH_TOKEN)
         # Process the response and return the status
         return process_response(response, log_level=self.log)
+    
+    def get_task_list(self):
+        # Send a GET request to retrieve the robot's status
+        url = f"{self.base_url}/timelines"
+        response = send_get_request(url, auth=self.AUTH_TOKEN)
+        # Process the response and return the status
+        processed_response = process_response(response, log_level=self.log)
+        task_names = []
+        for i in range(len(processed_response)):
+            task_names.append(processed_response[i]['id'])
+        print(task_names)
+        return task_names
 
     def open_brakes(self):
         # Send a POST request to open the robot's brakes
@@ -98,7 +110,7 @@ class Robot:
 if __name__ == "__main__":
     ROBOT_IP = "192.168.1.195"
     AUTH = "authorization=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZnJhIiwicm9sZSI6eyJhdXRob3JpemF0aW9uIjpbeyJwZXJtaXNzaW9uIjoiUmVhZFdyaXRlIiwicmVzb3VyY2UiOiJUYXNrcyJ9LHsicGVybWlzc2lvbiI6IlJlYWRXcml0ZSIsInJlc291cmNlIjoiU2tpbGxzIn0seyJwZXJtaXNzaW9uIjoiUmVhZFdyaXRlIiwicmVzb3VyY2UiOiJQYXJhbWV0ZXJzIn0seyJwZXJtaXNzaW9uIjoiUmVhZFdyaXRlIiwicmVzb3VyY2UiOiJFeGVjdXRpb24ifSx7InBlcm1pc3Npb24iOiJSZWFkV3JpdGUiLCJyZXNvdXJjZSI6IlN0YXR1cyJ9LHsicGVybWlzc2lvbiI6IlJlYWRXcml0ZSIsInJlc291cmNlIjoiQnVuZGxlcyJ9LHsicGVybWlzc2lvbiI6IlJlYWRXcml0ZSIsInJlc291cmNlIjoiU2NyaXB0cyJ9LHsicGVybWlzc2lvbiI6IlJlYWRXcml0ZSIsInJlc291cmNlIjoiQWRtaW4ifSx7InBlcm1pc3Npb24iOiJSZWFkV3JpdGUiLCJyZXNvdXJjZSI6IlNhZmV0eSJ9XSwibmFtZSI6ImFkbWluIn19.rGp0QvF1_d96hbWbetPr4Eg7DVJ4drO8cz8bpUqONj17_LkezZSakqZ72u3gXjXaInA6OM97m-bu4thgFdgblw"
-    robot = Robot(ROBOT_IP, cookie=AUTH, logging="DEBUG")
+    robot = Robot(ROBOT_IP, cookie=AUTH, logging="INFO")
 
     while True:
         number = int(input("\n\nPlease enter a number from 1 to 7, with the following actions to happen: \
@@ -108,7 +120,8 @@ if __name__ == "__main__":
                    \n4: Get the robot status \
                    \n5: Get the system status \
                    \n6: Start the task 'matthieu'. CAREFUL the robot will move! \
-                   \n7: Create new task\n"))
+                   \n7: Create new task \
+                   \n8: Get Task List\n"))
         
         switcher = {
             1: robot.open_brakes,
@@ -117,7 +130,8 @@ if __name__ == "__main__":
             4: robot.get_robot_status,
             5: robot.get_system_status,
             6: robot.start_task, 
-            7: robot.UI_create_task
+            7: robot.UI_create_task,
+            8: robot.get_task_list
         }
 
         # Get the function based on the input number
